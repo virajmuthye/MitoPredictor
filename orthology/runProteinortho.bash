29511136c895f824e66afbba88a7950cee646174
Viraj Muthye <vrmuthye@iastate.edu>
2019-04-03 16:06:04 -0500

    Add files via upload

---
+++ orthology/runProteinortho.bash
@@ -0,0 +1,13 @@
+module use /opt/rit/modules
+module load ncbi-blast/2.4.0+
+module load perl
+module load python
+
+perl /home/vrmuthye/mitopredictor/proteinortho_v5.16b/proteinortho5.pl -project=mito *.pep
+
+
+
+
+
+
+
