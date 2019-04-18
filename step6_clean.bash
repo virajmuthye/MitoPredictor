b03f16c18c47d131e525684ecce9102a9541d72a
Viraj Muthye <vrmuthye@iastate.edu>
2019-04-18 06:17:18 -0500

    Add files via upload

---
+++ step6_clean.bash
@@ -0,0 +1,24 @@
+cd prep
+rm *.fasta
+rm renamed.pep
+rm query*
+cd ..
+
+cd orthology
+rm mito.*
+rm orthology.matrix
+rm query*
+cd ..
+
+cd mts
+rm final_mts.matrix
+rm query*
+cd ..
+
+cd domain
+rm domain_final.matrix
+rm query*
+rm *mito_prot
+cd ..
+
+rm -rf concatanate
